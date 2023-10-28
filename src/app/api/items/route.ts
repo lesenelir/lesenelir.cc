cbc835490a4bc1b5c440080b08bdf7ddd7bf1ed5
lesenelir <hi@lesenelir.me>
2023-10-28 20:03:00 +0800

    feat: api route handler

--- src/app/api/items/route.ts
+++ src/app/api/items/route.ts
@@ -1,10 +1,15 @@
 // The route function must be named as the HTTP method name
-export async function GET() {
+export async function POST() {
   const options = {
-    method: 'GET',
-    // headers: {
-    //   'Content-Type': 'application/json',
-    // }
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      title: 'foo',
+      body: 'bar',
+      userId: 1,
+    })
   }
 
   const res = await fetch('https://jsonplaceholder.typicode.com/posts', options)
