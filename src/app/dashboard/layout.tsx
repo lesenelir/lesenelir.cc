eabfc97fe62a753f6541f0a85bbe84a2d52b29a9
lesenelir <hi@lesenelir.me>
2023-10-28 18:35:18 +0800

    feat: route groups

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -6,6 +6,7 @@ interface IProps {
 
 export default function DashBoardLayout(props: IProps) {
   // page or nested layout
+  // layout 中的 children 是 当前 route segment 的 page 和 nested 组件
   const {children} = props
 
   return (
